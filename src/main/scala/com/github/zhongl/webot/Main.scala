a59ecd4f2e0e5de86a87251d0eca9197a4830c39
zhongl <zhong.lunfu@gmail.com>
2021-08-03 16:57:03 +0800

    Introduce predef code avoid importing.

--- src/main/scala/com/github/zhongl/webot/Main.scala
+++ src/main/scala/com/github/zhongl/webot/Main.scala
@@ -5,6 +5,7 @@ import mainargs._
 import ammonite._
 import ammonite.main._
 import java.io.PrintStream
+import java.util.jar
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -15,7 +16,6 @@ object Main {
     val url = service.getUrl.toString
     sys.props.addOne("webdriver.remote.server" -> url)
     Console.err.println(s"Chrome Service Started At: $url")
-    ammonite.Main.main(args)
 
     val printErr = new PrintStream(System.err)
     val printOut = new PrintStream(System.out)
@@ -30,28 +30,8 @@ object Main {
         sys.exit(1)
 
       case Right(cfg) =>
-        val core = Config.Core(
-          noDefaultPredef = Flag(true),
-          silent = cfg.opts.silent,
-          watch = cfg.opts.watch,
-          bsp = Flag(false),
-          home = os.Path(System.getProperty("user.home")) / ".webot",
-          color = cfg.opts.color,
-          thin = Flag(true),
-          help = Flag(false)
-        )
-        val predef = Config.Predef(
-          "import $ivy.`com.github.zhongl:webot_2.13:0.1.0`, com.github.zhongl._, webot._ , webot.selenium._",
-          Flag(true)
-        )
-        val repl = Config.Repl(
-          "Welcome to Webot REPL",
-          Flag(true),
-          Flag(false)
-        )
-
         val runner = new MainRunner(
-          Config(core, predef, repl, cfg.rest: _*),
+          config(cfg),
           printOut,
           printErr,
           System.in,
@@ -76,6 +56,31 @@ object Main {
         }
     }
 
+  }
+
+  private def config(cfg: Cfg) = {
+    val version = Option(getClass().getPackage().getImplementationVersion()).getOrElse("0.1.0") // Caution !!!
+    val core = Config.Core(
+      noDefaultPredef = Flag(false),
+      silent = cfg.opts.silent,
+      watch = cfg.opts.watch,
+      bsp = Flag(false),
+      home = os.Path(System.getProperty("user.home")) / ".webot",
+      color = cfg.opts.color,
+      thin = Flag(true),
+      help = Flag(false)
+    )
+    val predef = Config.Predef(
+      s"import $$ivy.`com.github.zhongl:webot_2.13:$version`, com.github.zhongl._, webot._ , webot.selenium._",
+      Flag(true)
+    )
+    val repl = Config.Repl(
+      s"Welcome to Webot REPL $version",
+      Flag(true),
+      Flag(false)
+    )
+
+    Config(core, predef, repl, cfg.rest: _*)
   }
 
   @main
